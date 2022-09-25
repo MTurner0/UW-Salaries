c8e0bfbb4d672bbd0aafdc5674a8315b2d157d8e
MTurner0 <margaret.turner1416@gmail.com>
2022-09-25 14:03:15 -0500

    Init commit.

---
+++ R/EDA.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+data <- read_csv("../../data/forbidden.csv") %>% 
+  mutate(
+    `Total Pay` = `Total Pay` %>%
+      gsub(x = ., pattern = "[$]", replacement = "") %>%
+      gsub(x = ., pattern = ",", replacement = "") %>%
+      as.numeric(),
+    `Start Date` = `Start Date` %>%
+      lubridate::mdy()
+  )
+
+data %>% 
+  filter(!str_starts(`Dept Description`, "Ath/")) %>% 
+  ggplot() +
+  geom_jitter(aes(x = factor(`Fiscal year`), y = `Total Pay`), shape = "x", width = 0.2) +
+  labs(x = "Fiscal Year", y = "Total Pay (USD)") +
+  theme_bw()
+  
